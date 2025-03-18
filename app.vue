7a2c1ea35316e6248c5bb7824256181311824ecd
Jose Carrizales <jrafaelca@gmail.com>
2025-03-18 10:01:42 -0300

    first commit

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <UApp>
+    <MyLayerComponent>
+      <span class="text-4xl font-semibold text-(--ui-primary)">
+        Hello Nuxters!
+      </span>
+    </MyLayerComponent>
+  </UApp>
+</template>
